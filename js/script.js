c437de6a6eeb693b2f5d5771493ffc13c9628232
shavetapujani <shavetapujani@gmail.com>
2023-01-30 10:39:04 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Mobile navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
